3b0601f6b9d9863975f50cb599dbff54abec07f7
Mariano Conti <nanexcool@gmail.com>
2018-01-10 02:16:23 -0300

    Add instructions for MEW

--- src/Faq.jsx
+++ src/Faq.jsx
@@ -20,6 +20,12 @@ export default () => {
           DAI Stablecoin v1.0 token: <a href="https://etherscan.io/token/0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359" target="_blank" rel="noopener noreferrer">0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359</a>
         </p>
         <h5>
+          Instructions for redeeming using MyEtherWallet
+        </h5>
+        <p>
+          If you have a Ledger or Trezor or want to manually control the redeem process, you can use MyEtherWallet to do so. <a href="https://docs.google.com/document/d/1Rh6GH3TkKrbEmzeEw6R3TrG2hmkmmqva5dTKzxftL8k/edit?usp=sharing" target="_blank" rel="noopener noreferrer">This Google Doc has detailed instructions.</a>
+        </p>
+        <h5>
           Can I see the source code?
         </h5>
         <p>
